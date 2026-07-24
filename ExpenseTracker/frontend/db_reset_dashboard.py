c14edcd2de379fdef557c84d2bbbcca891b593ab
soumyadip <soumyadiperikson@gmail.com>
2026-07-25 00:45:51 +0530

    Made changes to include authorization heaer in API call to reset expense table for the logged in user and remove userid attribute previously used for API calling

--- ExpenseTracker/frontend/db_reset_dashboard.py
+++ ExpenseTracker/frontend/db_reset_dashboard.py
@@ -1,9 +1,10 @@
 import requests
 import streamlit as st
+from ExpenseTracker.frontend.auth_dashboard import get_auth_headers
 
 API_url = 'http://127.0.0.1:8000'
 
-def reset(userid : str):
+def reset():
     '''
     UI Function that resets/clears database corresponding to a user on Simpex dashboard.
     :return:
@@ -12,7 +13,7 @@ def reset(userid : str):
         reset_database = st.toggle('Reset Database' ,help='This cleans up all entries in the database.')
 
         if reset_database:
-            response = requests.post(f"{API_url}/reset/{userid}")
+            response = requests.post(f"{API_url}/reset/",headers=get_auth_headers())
             if response.status_code == 200:
                 reset_message = response.json()
                 st.write(reset_message['message'])
