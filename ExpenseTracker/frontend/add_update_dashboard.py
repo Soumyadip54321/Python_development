5fd2bb45ba264adb84b09df2da7b9abd1e69e79d
soumyadip <soumyadiperikson@gmail.com>
2026-07-30 00:40:32 +0530

    Added a dialog modal that seeks user confirmation for permanent data deletion of a specific date

--- ExpenseTracker/frontend/add_update_dashboard.py
+++ ExpenseTracker/frontend/add_update_dashboard.py
@@ -1,13 +1,42 @@
 import requests
-import pandas as pd
 import streamlit as st
 import datetime as dt
+import time
 from ExpenseTracker.frontend.auth_dashboard import get_auth_headers
 
 from sqlalchemy import false
 
 category_types = ['Entertainment','Shopping','Food','Other','Rent','Electronics','Groceries']
 API_url = 'http://127.0.0.1:8000'
+
+@st.dialog("Delete expenses")
+def delete_expenses(date : dt.date):
+    '''
+    Function to delete expenses permanently for a date.
+    :param expenses:
+    :return:
+    '''
+    st.warning("⚠️ This will permanently delete all expenses for this date. Do you intend to proceed?")
+    left,col1,col2,right = st.columns([1,3,3,1])
+
+    with col1:
+        if st.button("Delete expense"):
+            delete_response = requests.delete(f"{API_url}/delete_expenses/{date}", headers=get_auth_headers())
+            if delete_response.status_code == 200:
+                st.badge(f"{delete_response.json()['message']}", color='green', icon=":material/check:")
+            else:
+                st.error('Failed to delete')
+            st.session_state.data_loaded = False
+            time.sleep(5)
+            st.rerun()
+
+    with col2:
+        if st.button("Cancel"):
+            st.write('Operation cancelled.')
+
+            st.session_state.data_loaded = False
+            time.sleep(5)
+            st.rerun()
 
 def add_update():
     '''
@@ -88,16 +117,22 @@ def add_update():
             # prepare data for database update
             user_expense_info = {'expenses': expenses_on_date}
 
-            submitted = st.form_submit_button('Save', type='primary')
+            update_data = st.form_submit_button('Save', type='primary')
+            delete_data = st.form_submit_button('Delete', type='secondary')
             # st.write(user_expense_info)
 
-            if submitted:
-
+            # update expense data in the database for a specific date
+            if update_data:
                 # submit filtered expenses to database using API
-                post_response = requests.post(f"{API_url}/expenses/{date}", json=user_expense_info, headers=get_auth_headers())
-                if post_response.status_code == 200:
+                put_response = requests.put(f"{API_url}/expenses/{date}", json=user_expense_info, headers=get_auth_headers())
+                if put_response.status_code == 200:
                     st.badge("Success: data saved.", color='green', icon=":material/check:")
                 else:
                     st.error('Failed to post')
-
                 st.session_state.data_loaded = False
+                time.sleep(5)
+                st.rerun()
+
+            # remove expense data from the database corresponding to a date. On clicking it opens a dialog box to re-confirm deletion before proceeding.
+            if delete_data:
+                delete_expenses(date)
