2c147a09097b74c004c6c89d6fb106a728b6804e
soumyadip <soumyadiperikson@gmail.com>
2026-07-30 00:38:53 +0530

    Migrated code from synchronous to asynchronous for scalability and latency improvement

--- ExpenseTracker/backend/db_interaction.py
+++ ExpenseTracker/backend/db_interaction.py
@@ -1,58 +1,62 @@
 '''
 Script to demonstrate CRUD - Create Read Update Delete with MySQL database in python.
 '''
-import pymysql
+import asyncio
+from typing import Tuple, List
 from dotenv import load_dotenv
 import os
-from contextlib import contextmanager
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker
+)
 from ExpenseTracker.backend.logging_setup import setup_logger
 from datetime import date
 from fastapi import HTTPException
 from ExpenseTracker.backend.auth import hash_password, verify_password
+from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError
 
 # loads .env file
 load_dotenv()
 
+DATABASE_URL = (
+    f"mysql+asyncmy://{os.getenv('DB_USER')}:{os.getenv('MYSQL_password')}"
+    f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+)
+
+# create database manager that maintains a pool of reusable connections that a request can borrow from and return it back to pool once done.
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+)
+
+# create session class - Connection + Cursor + Transaction Manager
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
 # create a custom logger
 logger = setup_logger("db_interaction.log", "db_interaction.log", "INFO")
 
-@contextmanager
-def get_db_cursor(to_be_commited=False):
+@asynccontextmanager
+async def get_db_cursor(to_be_commited=False):
     '''
-    Function to obtain database cursor
+    Creates an asynchronous database session.
     :return:
     '''
     logger.info('Connecting to MySQL database')
 
-    # establish connection with mysql database and make connection in a way that cursor returns result in dictionary format
-    mysql_connection = pymysql.connect(
-        host="localhost",
-        user="root",
-        password=os.getenv("MYSQL_password"),
-        database="expense_manager",
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    # This borrows from the pool of connections one connection that engine created. When done it auto returns the connection back to the pool.
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
 
-    # check if connected
-    try:
-        # If connection succeeds put query by setting up a cursor and display results in form of dictionary
-        cursor = mysql_connection.cursor()
-        print("Connected to MySQL database")
-        yield cursor
-        # In case of any change made to the database the change is temporarily stored unless commited.
-        if to_be_commited:
-            mysql_connection.commit()
-
-    except Exception as e:
-        mysql_connection.rollback()
-        raise e
-    finally:
-        # close cursor
-        cursor.close()
-        # close connection
-        mysql_connection.close()
-
-def fetch_all_records():
+async def fetch_all_records():
     '''
     Function to fetch all records from database table
     :return:
@@ -60,13 +64,13 @@ def fetch_all_records():
     logger.info('Fetching all records from database table')
 
     # fetch db cursor
-    with get_db_cursor() as cursor:
+    async with get_db_cursor() as session:
         # fetch all expenses
-        cursor.execute("select * from expenses;")
-        expenses = cursor.fetchall()
+        result = await session.execute(text("select * from expenses;"))
+        expenses = result.mappings().all()
         return expenses
 
-def fetch_expenses_for_date(expense_date: date, userid : int):
+async def fetch_expenses_for_date(expense_date: date, userid : int):
     '''
     Function to fetch all expenses for a specific user and date
     :param expense_date: Expense date
@@ -76,15 +80,20 @@ def fetch_expenses_for_date(expense_date: date, userid : int):
     logger.info('Fetching all expenses for date')
 
     # fetch db cursor
-    with get_db_cursor() as cursor:
+    async with get_db_cursor() as session:
         # fetch all expenses
-        cursor.execute("select * from expenses where expense_date = %s and id = %s;", (expense_date,userid))
-        expenses = cursor.fetchall()
+        # cursor.execute("select * from expenses where expense_date = %s and id = %s;", (expense_date,userid))
+        # expenses = cursor.fetchall()
+        result = await session.execute(text("select * from expenses where expense_date = :expense_date and id = :id"),{"expense_date":expense_date,"id":userid})
+        expenses = result.mappings().all()
         return expenses
 
-def insert_into_database(expense_date,userid,amt,cat,notes):
+async def update_expenses_in_database(expenses : List[Tuple]):
     '''
-    Function to insert entry into database
+    Function to update all expenses in database for the date. This initially removes all existing expenses corresponding to date
+    and then puts new expenses.
+    In case old records are removed while new records couldn't be added the deletion is rolled back.
+
     :param expense_date: Expense date
     :param userid: User ID
     :param amt: Amount spent
@@ -92,12 +101,31 @@ def insert_into_database(expense_date,userid,amt,cat,notes):
     :param notes: Description of the expense
     :return:
     '''
-    logger.info(f'Inserting data into database corresponding to expense date {expense_date} and user {userid}')
+    logger.info(f'Delete existing record for user {expenses[0][0]} corresponding to date {expenses[0][1]} and override new expenses')
 
-    with get_db_cursor(to_be_commited=True) as cursor:
-        cursor.execute("insert into expenses (id ,expense_date, amount, category, notes) values (%s, %s, %s, %s, %s);", (userid,expense_date,amt,cat,notes))
+    userid = expenses[0][0]
+    expense_date = expenses[0][1]
 
-def delete_records_from_database_for_a_date(expense_date,userid):
+    async with get_db_cursor() as session:
+        # delete existing records if any on the date - pause coroutine whilst session executes query and commits it
+        await session.execute(text("delete from expenses where id = :id and expense_date = :expense_date;"), {"id":userid,"expense_date":expense_date})
+        await session.commit()
+
+        # insert new records for the date - pause the coroutine while session executes the query and commits it
+        await session.execute(text("insert into expenses (id ,expense_date, amount, category, notes) values (:id, :expense_date, :amount, :category, :notes);"),
+                              [
+                                  {
+                                      "id":expense[0],
+                                      "expense_date":expense[1],
+                                      "amount":expense[2],
+                                      "category":expense[3],
+                                      "notes":expense[4]
+                                  }
+                                  for expense in expenses
+                              ])
+        await session.commit()
+
+async def delete_records_from_database_for_a_date(expense_date,userid):
     '''
     Function to delete from database entries related to expense date.
     :param expense_date: Remove a record from database with expense date.
@@ -105,10 +133,11 @@ def delete_records_from_database_for_a_date(expense_date,userid):
     '''
     logger.info(f'Deleting data from database corresponding to expense date {expense_date} and user {userid}')
 
-    with get_db_cursor(to_be_commited=True) as cursor:
-        cursor.execute("delete from expenses where expense_date = %s and id = %s;", (expense_date,userid))
+    async with get_db_cursor() as session:
+        await session.execute(text("delete from expenses where expense_date = :expense_date and id = :id;"), {"expense_date":expense_date,"id":userid})
+        await session.commit()
 
-def reset_database(user_id: int):
+async def reset_database(user_id: int):
     '''
     Function to remove all expense entries from the database corresponding to a specific user.
     :param user_id: User ID
@@ -116,12 +145,11 @@ def reset_database(user_id: int):
     '''
     logger.info('Resetting database')
 
-    with get_db_cursor(to_be_commited=True) as cursor:
-        cursor.execute("delete from expenses where id = %s;", (user_id,))
-        # cursor._connection.commit()
+    async with get_db_cursor() as session:
+        await session.execute(text("delete from expenses where id = :id;"), {"id":user_id})
+        await session.commit()
 
-
-def fetch_expenses_summary(expense_date1,expense_date2,userid : int):
+async def fetch_expenses_summary(expense_date1,expense_date2,userid : int):
     '''
     Function to fetch all expenses between two dates across different categories.
     :param expense_date1: Start date
@@ -132,12 +160,12 @@ def fetch_expenses_summary(expense_date1,expense_date2,userid : int):
 
     logger.info('Fetching all expenses between dates')
 
-    with get_db_cursor() as cursor:
-        cursor.execute("select category,sum(amount) as total from expenses where id = %s and expense_date between %s and %s group by category order by total desc;",(userid,expense_date1,expense_date2,))
-        expenses = cursor.fetchall()
+    async with get_db_cursor() as session:
+        result = await session.execute(text("select category,sum(amount) as total from expenses where id = :id and expense_date between :date1 and :date2 group by category order by total desc;"),{"id":userid,"date1":expense_date1,"date2":expense_date2})
+        expenses = result.mappings().all()
         return expenses
 
-def register_user(username,password:str):
+async def register_user(username,password:str):
     '''
     Function to insert new user info into database for authentication against backend-driven hashed pwd.
     In addition it also fetches database insertion failure when duplicate username is found.
@@ -149,13 +177,14 @@ def register_user(username,password:str):
 
     # store username and hashed-pwd in database. In case of duplicate username raises error.
     try:
-        with get_db_cursor(to_be_commited=True) as cursor:
-            cursor.execute('insert into LOGGED_USERS (USERNAME, PASSWORD) values (%s, %s);', (username, hash_password(password),))
-    except pymysql.err.IntegrityError as err:
-        if err.args[0] == 1062:
+        async with get_db_cursor() as session:
+            await session.execute(text('insert into LOGGED_USERS (USERNAME, PASSWORD) values (:username, :pwd);'), {"username":username, "pwd":hash_password(password)})
+            await session.commit()
+    except IntegrityError as err:
+        if err.orig.args[0] == 1062:
             raise HTTPException(status_code=409, detail="User already exists!. Please choose a different username!")
 
-def check_for_logged_user(username,pwd):
+async def check_for_logged_user(username,pwd):
     '''
     Function to check if user exists in database. It fetches hashed pwd against user from the database
     and checks whether hashed pwd matches plain-text pwd provided by user at login time and then grants access.
@@ -166,9 +195,9 @@ def check_for_logged_user(username,pwd):
     logger.info('Checking if user exists in database')
 
     try:
-        with get_db_cursor() as cursor:
-            cursor.execute('select USERNAME,PASSWORD,ID from LOGGED_USERS where USERNAME = %s;', (username,))
-            result = cursor.fetchone()
+        async with get_db_cursor() as session:
+            result = await session.execute(text('select USERNAME,PASSWORD,ID from LOGGED_USERS where USERNAME = :username;'), {"username":username})
+            result = result.mappings().first()
 
         # on successful data fetch from database perform pwd and username validation
         if verify_password(pwd, result['PASSWORD']) and result['USERNAME'] == username:
@@ -178,29 +207,28 @@ def check_for_logged_user(username,pwd):
         logger.exception('User not found in database.')
         raise HTTPException(status_code=404, detail="User not found in database.")
 
-def check_for_duplicate_username(username):
+async def check_for_duplicate_username(username):
     '''
     Function that checks whether there exists duplicate username in database.
     :param username: username provided by user at the time of registration.
     :return:
     '''
     logger.info('Checking if username exists in database')
-    with get_db_cursor() as cursor:
-        cursor.execute('select USERNAME from LOGGED_USERS where USERNAME = %s;',(username,))
-        result = cursor.fetchone()
+    async with get_db_cursor() as session:
+        result = await session.execute(text('select USERNAME from LOGGED_USERS where USERNAME = :username;'),{"username":username})
+        result = result.mappings().first()
 
         if result:
             return True
         return False
 
 # if __name__ == '__main__':
-    # print(fetch_all_records())
+    # print(asyncio.run(fetch_all_records()))
     # fetch_expenses_for_date('2024-08-02')
     # insert_into_database('2025-01-01',5000.0,'Shopping','Purchased apparels')
     # delete_from_database("2025-01-01")
     # fetch_expenses_for_date('2025-01-01')
     # fetch_expenses_categorywise_between_dates("2024-08-02","2024-12-31")
-    # print(check_for_logged_user('sikdsou','Christiano#7'))
+    # print(asyncio.run(check_for_logged_user('sikdsou','Christiano#7')))
     # print(check_for_duplicate_username('messi'))
     # print(register_user(username="sikdsou",password="Tkinter@10"))
-
